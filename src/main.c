2d9a95e61cdbe0e19618dc0b3eff553cb89662ba
banjo-bytes <243734090+banjo-bytes@users.noreply.github.com>
2026-03-01 23:19:42 +1030

    Created main function

---
+++ src/main.c
@@ -0,0 +1,19 @@
+################################################################################
+# File:    main.c
+# Author:  banjo-bytes
+# Date:    2026-03-01
+################################################################################
+
+/*	Usage: ./phil N td te ts [Nm]
+	N - Number of philosophers
+	td - time to die (ms)
+	te - time to eat (ms)
+	ts - time to sleep (ms)
+	[Nm] - number of meals before sim stops [optional]
+*/
+int	main(int argc, char **argv)
+{
+	if (argc < 5 || arcg > 7 || arg_check(argv))
+		return (EXIT_FAILURE);
+	return (0);
+}
