367eb66048e62a385cd18a8b92d681795d1ce60a
banjo-bytes <243734090+banjo-bytes@users.noreply.github.com>
2026-03-02 16:29:34 +1030

    Added brainstorming notes in main.c

--- src/main.c
+++ src/main.c
@@ -5,12 +5,18 @@
 ################################################################################ */
 
 #include <stdlib.h>
+
 /*	Usage: ./phil N td te ts [Nm]
 	N - Number of philosophers
 	td - time to die (ms)
 	te - time to eat (ms)
 	ts - time to sleep (ms)
 	[Nm] - number of meals before sim stops [optional]
+*/
+
+/* Create a thread:
+Printing status to stdout => Each philosopher needs access to global time elapsed timestamp, its own ID/no., left+right fork, mutex lock for the stdout printing buffer
+Simulation tracking => Each philosopher needs to track number of meals eaten and Nm (if provided), must know td, te, ts
 */
 int	main(int argc, char **argv)
 {
