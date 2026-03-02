bf36cd03584bbc5b0433aa3b90e1a8fbf8cf3b78
banjo-bytes <243734090+banjo-bytes@users.noreply.github.com>
2026-03-02 19:00:30 +1030

    Added philo.h and created initial struct types

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 # Date:    2026-03-01
 ################################################################################ */
 
+#include "philo.h"
 #include <stdlib.h>
 
 /*	Usage: ./phil N td te ts [Nm]
@@ -18,6 +19,24 @@
 Printing status to stdout => Each philosopher needs access to global time elapsed timestamp, its own ID/no., left+right fork, mutex lock for the stdout printing buffer
 Simulation tracking => Each philosopher needs to track number of meals eaten and Nm (if provided), must know td, te, ts
 */
+
+/* Iteration 1: Forget timestamps, assume inputs correct, assume functions return successfully, only print to stdout the philosopher number and status*/
+
+typedef struct	s_shared {
+	pthread_mutex_t	print_mtx;
+	pthread_mutex_t	*forks;
+	int				max_meals;
+	int				td;
+	int				te;
+	int				ts;
+}				t_shared;
+
+typedef struct	s_philo {
+	t_shared	*shared;
+	int			phil_id;
+	int			meals_eaten;
+}				t_philo;
+
 int	main(int argc, char **argv)
 {
 	if (argc < 5 || argc > 7) //TODO: Add || arg_check(argv))
